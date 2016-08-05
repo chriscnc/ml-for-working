c81b4bc2153f8b2410d34bd112dd591fc9ce7794
Chris <chris.cornelison@gmail.com>
2016-08-05 13:55:16 -0500

    Updates

---
+++ ch4-test.ml
@@ -0,0 +1,111 @@
+
+
+let birnam = 
+  Br("The", 
+     Br("wood", 
+        Lf,
+        Br("of", 
+           Br("Birnam", 
+              Lf, 
+              Lf), 
+           Lf)),
+     Lf)
+
+let tree2 =
+  Br(2, Br(1, Lf, Lf), Br(3, Lf, Lf))
+
+let tree5 =
+  Br(5, Br(6, Lf, Lf), Br(7, Lf, Lf))
+
+let tree4 =
+  Br(4, tree2, tree5)
+
+
+let test_obvious_bal () = 
+  let b1 = Lf and
+  b2 = Br(42,Lf,Lf) and
+  b3 = Br(42, Br(42,Lf,Lf), Br(42,Lf,Lf)) and
+  b4 = Br(42, Br(42,Lf,Lf), Lf) and
+  b5 = Br(42, Br(42,Br(42,Lf,Lf), Br(42,Lf,Lf)), Lf) and
+  b6 = Br(42, Br(42,Br(42,Br(42,Lf,Lf),Lf),Lf)
+            , Br(42,Br(42,Br(42,Lf,Lf),Lf),Lf))
+  in assert (obvious_bal b1);
+  assert (obvious_bal b2);
+  assert (obvious_bal b3);
+  assert (obvious_bal b4);
+  assert (not (obvious_bal b5));
+  assert (not (obvious_bal b6));
+  true;;
+
+test_obvious_bal ();;
+
+let test_balanced () = 
+  let b1 = Lf and
+  b2 = Br(42,Lf,Lf) and
+  b3 = Br(42, Br(42,Lf,Lf), Br(42,Lf,Lf)) and
+  b4 = Br(42, Br(42,Lf,Lf), Lf) and
+  b5 = Br(42, Br(42,Br(42,Lf,Lf), Br(42,Lf,Lf)), Lf) and
+  b6 = Br(42, Br(42,Br(42,Br(42,Lf,Lf),Lf),Lf)
+            , Br(42,Br(42,Br(42,Lf,Lf),Lf),Lf))
+  in assert (balanced b1);
+  assert (balanced b2);
+  assert (balanced b3);
+  assert (balanced b4);
+  assert (not (balanced b5));
+(*  assert (not (balanced b6)); *)
+  true;;
+
+test_balanced ();;
+
+
+let test_reflected () = 
+  let ref_tree2 = reflect tree2 
+  in assert (reflected ref_tree2 tree2);
+  assert (not (reflected tree2 tree2));
+  true;;
+
+test_reflected ();;
+
+
+
+let test_tree_enums () =
+  assert ((preorder birnam) = ["The"; "wood"; "of"; "Birnam"]);
+  assert ((preorder tree4) = [4;2;1;3;5;6;7]);
+  assert ((inorder birnam) = ["wood"; "Birnam"; "of"; "The"]);
+  assert ((inorder tree4) = [1;2;3;4;6;5;7]);
+  assert ((postorder birnam) = ["Birnam";"of";"wood";"The"]);
+  assert ((postorder tree4) = [1;3;2;6;7;5;4]);
+  true;;
+
+test_tree_enums ();;
+
+
+let test_tree_enums_optimized () =
+  assert ((preord birnam []) = ["The"; "wood"; "of"; "Birnam"]);
+  assert ((preord tree4 []) = [4;2;1;3;5;6;7]);
+  assert ((inord birnam []) = ["wood"; "Birnam"; "of"; "The"]);
+  assert ((inord tree4 []) = [1;2;3;4;6;5;7]);
+  assert ((postord birnam []) = ["Birnam";"of";"wood";"The"]);
+  assert ((postord tree4 []) = [1;3;2;6;7;5;4]);
+  true;;
+
+test_tree_enums_optimized ();;
+
+
+
+let test_make_bal_tree () =
+  assert (tree4 = (balpre [4;2;1;3;5;6;7]));
+  assert (tree4 = (balin [1;2;3;4;6;5;7]));
+  assert (tree4 = (balpos [1;3;2;6;7;5;4]));
+  true;;
+
+test_make_bal_tree ();;
+
+
+
+
+
+
+  
+
+
--- ch4.ml
+++ ch4.ml
@@ -1,44 +1,148 @@
+let last lst = List.hd (List.rev lst)
+
+let rec take lst n = match lst with
+    [] -> []
+  | x::xs -> 
+    if n > 0 then x::(take xs (n-1))
+    else []
+
+
+let rec drop lst n = match lst with
+    [] -> []
+  | x::xs ->
+    if n > 0 then (drop xs (n-1))
+    else lst
+
+
+let explode s = 
+  let rec f i acc = 
+    if i > 0 then f (i-1) (s.[i-1]::acc)
+    else acc
+  in
+  f (String.length s) []
+
+
+let implode l =
+  let res = Bytes.make (List.length l) ' ' in
+  let rec f i l = match l with
+      [] -> res
+    | x::xs -> Bytes.set res i x; f (i + 1) xs
+  in
+  f 0 l
+
 
 type 'a tree = 
     Lf
   | Br of 'a * 'a tree * 'a tree
 
-let birnam = 
-  Br("The", 
-     Br("wood", 
-        Lf,
-        Br("of", 
-           Br("Birnam", 
-              Lf, 
-              Lf), 
-           Lf)),
-     Lf)
+module Tree = 
+struct
+  let rec size = function
+      Lf          -> 0
+    | Br(v,t1,t2) -> 1 + size t1 + size t2
 
-let tree2 =
-  Br(2, Br(1, Lf, Lf), Br(3, Lf, Lf))
+  let rec depth = function
+      Lf          -> 0
+    | Br(v,t1,t2) -> 1 + max (depth t1) (depth t2)
 
-let tree5 =
-  Br(5, Br(6, Lf, Lf), Br(7, Lf, Lf))
+  let rec reflect = function
+      Lf -> Lf
+    | Br(v,t1,t2) -> Br(v, reflect t2, reflect t1)
 
-let tree4 =
-  Br(4, tree2, tree5)
+  let rec preord t acc = match t with
+      Lf -> acc
+    | Br(v,t1,t2) -> v :: (preord t1 (preord t2 acc))
 
-let rec size = function
-    Lf          -> 0
-  | Br(v,t1,t2) -> 1 + size t1 + size t2
+  let rec inord t acc = match t with
+      Lf -> acc
+    | Br(v,t1,t2) -> inord t1 (v :: (inord t2 acc))
 
-let rec depth = function
-    Lf          -> 0
-  | Br(v,t1,t2) -> 1 + max (depth t1) (depth t2)
+  let rec postord t acc = match t with
+      Lf -> acc
+    | Br(v,t1,t2) -> postord t1 (postord t2 (v::acc))
 
+  let rec balpre = function
+      [] -> Lf
+    | x::xs -> 
+      let k = (List.length xs) / 2
+      in Br(x, balpre (take xs k), balpre (drop xs k))
+
+  let rec balin = function 
+      [] -> Lf
+    | xs ->
+      let k = (List.length xs) / 2 in
+      match (drop xs k) with
+        []    -> Lf
+      | y::ys -> Br(y, balin (take xs k), balin ys)
+
+  let rec balpos = function 
+      [] -> Lf
+    | xs ->
+      let k = (List.length xs) / 2 in
+      let ys = (drop xs k) in
+      match ys with
+        [] -> Lf
+      | zs -> 
+        let j = (List.length zs) - 1 in
+        let label = (last zs) in
+        Br(label, balpos (take xs k), balpos (take zs j))
+
+end
+
+module type Dictionary = 
+sig
+  type key
+  type 'a t
+  exception E of key
+  val empty : 'a t
+  val lookup : 'a t * key -> 'a
+  val insert : 'a t * key * 'a -> 'a t
+  val update : 'a t * key * 'a -> 'a t
+end
+
+module Dict : Dictionary = 
+struct
+
+  type key = string
+  type 'a t = (key * 'a) tree
+
+  exception E of key
+
+  val empty = Lf
+
+  let rec lookup = function
+      (Lf, b) -> raise E b
+    | (Br((a,x), t1, t2), b) ->
+      match String.compare(a,b) with
+        GREATER -> lookup(t1,b)
+      | EQUAL -> x
+      | LESS -> lookup(t2,b)
+
+  let rec insert = function
+      (Lf, b, y) -> Br((b,y), Lf, Lf)
+    | (Br((a,x), t1, t2), b, y) ->
+      match String.compare(a,b) with
+        GREATER -> Br((a,x), insert (t1,b,y), t2)
+      | EQUAL -> raise E b
+      | LESS -> Br((a,x), t1, insert (t2,b,y))
+
+  let rec update = function
+      (Lf, b, y) -> Br((b,y), Lf, Lf)
+    | (Br((a,x), t1, t2), b, y) ->
+      match String.compar(a,b) with
+        GREATER -> Br((a,x), update (t1,b,y), t2)
+      | EQUAL -> Br((a,y), t1, t2)
+      | LESS -> Br((a,x), t1, update (t2,b,y))
+
+end
+
+
+(*
 let rec comptree (k,n) = 
   if n = 0 then Lf
            else Br(k, comptree(2*k, n-1),
                       comptree(2*k+1, n-1))
 
-let rec reflect = function
-    Lf -> Lf
-  | Br(v,t1,t2) -> Br(v, reflect t2, reflect t1)
 
 (* Exercise 4.13
    Write a function compsame(x,n) to construct a complete binary tree
@@ -85,42 +189,6 @@ let balanced = function
                          t2s = size t2
       in (abs (t1s - t2s)) <= 1 
 
-let test_obvious_bal () = 
-  let b1 = Lf and
-  b2 = Br(42,Lf,Lf) and
-  b3 = Br(42, Br(42,Lf,Lf), Br(42,Lf,Lf)) and
-  b4 = Br(42, Br(42,Lf,Lf), Lf) and
-  b5 = Br(42, Br(42,Br(42,Lf,Lf), Br(42,Lf,Lf)), Lf) and
-  b6 = Br(42, Br(42,Br(42,Br(42,Lf,Lf),Lf),Lf)
-            , Br(42,Br(42,Br(42,Lf,Lf),Lf),Lf))
-  in assert (obvious_bal b1);
-  assert (obvious_bal b2);
-  assert (obvious_bal b3);
-  assert (obvious_bal b4);
-  assert (not (obvious_bal b5));
-  assert (not (obvious_bal b6));
-  true;;
-
-test_obvious_bal ();;
-
-let test_balanced () = 
-  let b1 = Lf and
-  b2 = Br(42,Lf,Lf) and
-  b3 = Br(42, Br(42,Lf,Lf), Br(42,Lf,Lf)) and
-  b4 = Br(42, Br(42,Lf,Lf), Lf) and
-  b5 = Br(42, Br(42,Br(42,Lf,Lf), Br(42,Lf,Lf)), Lf) and
-  b6 = Br(42, Br(42,Br(42,Br(42,Lf,Lf),Lf),Lf)
-            , Br(42,Br(42,Br(42,Lf,Lf),Lf),Lf))
-  in assert (balanced b1);
-  assert (balanced b2);
-  assert (balanced b3);
-  assert (balanced b4);
-  assert (not (balanced b5));
-(*  assert (not (balanced b6)); *)
-  true;;
-
-test_balanced ();;
-
 
 (* Exercise 4.15
    Write a function that determines whether two arbitrary trees t and u satisfy 
@@ -133,14 +201,6 @@ let rec reflected t u = match (t,u) with
     v1 = v2 && (reflected l1 r2) && (reflected r1 l2)
   | (Br(_,_,_), Lf) -> false
   | (Lf, Br(_,_,_)) -> false
-
-let test_reflected () = 
-  let ref_tree2 = reflect tree2 
-  in assert (reflected ref_tree2 tree2);
-  assert (not (reflected tree2 tree2));
-  true;;
-
-test_reflected ();;
 
 
 (* Exercise 4.16
@@ -183,43 +243,6 @@ let rec postorder = function
   | Br(v,t1,t2) -> postorder t1 @ postorder t2 @ [v]
 
 
-let test_tree_enums () =
-  assert ((preorder birnam) = ["The"; "wood"; "of"; "Birnam"]);
-  assert ((preorder tree4) = [4;2;1;3;5;6;7]);
-  assert ((inorder birnam) = ["wood"; "Birnam"; "of"; "The"]);
-  assert ((inorder tree4) = [1;2;3;4;6;5;7]);
-  assert ((postorder birnam) = ["Birnam";"of";"wood";"The"]);
-  assert ((postorder tree4) = [1;3;2;6;7;5;4]);
-  true;;
-
-test_tree_enums ();;
-
-
-let rec preord t acc = match t with
-    Lf -> acc
-  | Br(v,t1,t2) -> v :: (preord t1 (preord t2 acc))
-
-
-let rec inord t acc = match t with
-    Lf -> acc
-  | Br(v,t1,t2) -> inord t1 (v :: (inord t2 acc))
-
-
-let rec postord t acc = match t with
-    Lf -> acc
-  | Br(v,t1,t2) -> postord t1 (postord t2 (v::acc))
-
-
-let test_tree_enums_optimized () =
-  assert ((preord birnam []) = ["The"; "wood"; "of"; "Birnam"]);
-  assert ((preord tree4 []) = [4;2;1;3;5;6;7]);
-  assert ((inord birnam []) = ["wood"; "Birnam"; "of"; "The"]);
-  assert ((inord tree4 []) = [1;2;3;4;6;5;7]);
-  assert ((postord birnam []) = ["Birnam";"of";"wood";"The"]);
-  assert ((postord tree4 []) = [1;3;2;6;7;5;4]);
-  true;;
-
-test_tree_enums_optimized ();;
 
 
 (* Exercise 4.19
@@ -244,61 +267,9 @@ test_tree_enums_optimized ();;
    Not sure if this is what the question intends or not.
 *)
 
-let rec take lst n = match lst with
-    [] -> []
-  | x::xs -> 
-    if n > 0 then x::(take xs (n-1))
-    else []
 
 
-let rec drop lst n = match lst with
-    [] -> []
-  | x::xs ->
-    if n > 0 then (drop xs (n-1))
-    else lst
 
-
-let rec balpre = function
-    [] -> Lf
-  | x::xs -> 
-    let k = (List.length xs) / 2
-    in Br(x, balpre (take xs k), balpre (drop xs k))
-
-
-let rec balin = function 
-    [] -> Lf
-  | xs ->
-    let k = (List.length xs) / 2 in
-    match (drop xs k) with
-      []    -> Lf
-    | y::ys -> Br(y, balin (take xs k), balin ys)
-
-
-(* Exercise 4.21
-   Write a function to convert a postorder list of labels to a balanced tree. *)
-
-let last lst = List.hd (List.rev lst)
-
-let rec balpos = function 
-    [] -> Lf
-  | xs ->
-    let k = (List.length xs) / 2 in
-    let ys = (drop xs k) in
-    match ys with
-      [] -> Lf
-    | zs -> 
-      let j = (List.length zs) - 1 in
-      let label = (last zs) in
-      Br(label, balpos (take xs k), balpos (take zs j))
-
-
-let test_make_bal_tree () =
-  assert (tree4 = (balpre [4;2;1;3;5;6;7]));
-  assert (tree4 = (balin [1;2;3;4;6;5;7]));
-  assert (tree4 = (balpos [1;3;2;6;7;5;4]));
-  true;;
-
-test_make_bal_tree ();;
 (* Exercise 4.22
    The function balpre constructs one tree from a preorder list of labels. Write
    a function that, given a list of labels, constructs the list of all trees that
@@ -326,7 +297,4 @@ let all_balpre lst =
 
    What is wrong with this idea? *)
 
-
-
-
-
+*)
